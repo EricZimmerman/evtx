33e15e08e592fbcb26957f77e5fa5a01dbc05650
Eric Zimmerman <saericzimmerman@gmail.com>
2021-04-21 13:05:14 -0400

    migrate to new format

--- evtx.Test/Properties/AssemblyInfo.cs
+++ evtx.Test/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("evtx.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("evtx.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("95f2d7a0-54d6-435c-8567-35769d8850c8")]
-
-// MajorVersion information for an assembly consists of the following four values:
-//
-//      Major MajorVersion
-//      Minor MajorVersion
-//      Build Number
-//      MinorVersion
-//
-// You can specify all the values or you can default the Build and MinorVersion Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
